8ea6f1a43d5d045ca1284fa3773beb41c19e14c5
Riningan <Riningan@gmail.com>
2019-01-06 21:45:09 +0500

    version 1.0

--- sample/src/main/java/com/riningan/logger/SampleApp.java
+++ sample/src/main/java/com/riningan/logger/SampleApp.java
@@ -1,7 +1,6 @@
 package com.riningan.logger;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.riningan.util.Logger;
 import com.riningan.util.MessageType;
@@ -13,13 +12,11 @@ public class SampleApp extends Application {
     public void onCreate() {
         super.onCreate();
         Logger.Config
-                .setEnabled(false)
                 .removeApplicationId(BuildConfig.APPLICATION_ID)
                 .addPreffix(BuildConfig.VERSION_NAME)
                 .setOnMessageListener(new OnMessageListener() {
                     @Override
                     public void onNewMessage(MessageType type, String message) {
-//                        Log.d("!@#$%^&*(" + type.name(), message);
                     }
                 });
     }
